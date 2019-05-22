ab835e75614e88db8cd3f51c086cff4d042091a5
Ashley L Miller <amiller8@uoregon.edu>
2019-05-22 10:47:58 -0700

    Added shiny app

---
+++ lab4_shiny_app/app.R
@@ -0,0 +1,73 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(DT)
+library(forcats)
+
+gss_cat
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("General Social Science Survey: TV Data"),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("var",
+                         "Variable:", #label that appers on rendered html
+                         choices = c(
+                             "None" = "none",
+                             "Marital status" = "marital", 
+                             "Age" = "age", 
+                             "Identified race" = "race",
+                             "Reported income" = "rincome",
+                             "Political affiliation" = "partyid",
+                             "Identified religion" = "relig",
+                             "Identified denomination" = "denom"),
+                         selected = "none")
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("distPlot") #renders/produces "displot" (see below)
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+# input comes from ui
+
+server <- function(input, output) {
+    output$distPlot <- renderPlot({
+        p <- ggplot(gss_cat, aes(tvhours)) +
+            geom_histogram(colour = "#633974",
+                           fill = "#D2B4DE",
+                           alpha = 0.7) +
+            theme_minimal(base_size = 16) +
+            theme(axis.title = element_text(face = "bold"),
+                  strip.text = element_text(face = "bold"),
+                  axis.line = element_line(colour = "gray60")) +
+            scale_y_continuous(expand = c(0, 0)) +
+            labs(x = "TV Hours",
+                 y = "Count")
+        if(input$var != "none") {
+            p <- p + facet_wrap(input$var) # with strings we do not use ~
+        }
+        
+        p
+    })
+    
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
